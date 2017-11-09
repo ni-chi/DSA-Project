e98dc4be160eb80e2d8f22ff1a97b51f26ceafb1
Shreyas Shankar <shreyas@centralperk.nitk.ac.in>
2017-11-09 15:36:26 +0530

    AVL implimenttion

---
+++ main.py
@@ -0,0 +1,88 @@
+class ITNode :
+	def __init__(self, a = None, b = None) :
+		self.parent=None
+		self.low = a
+		self.high = b
+		self.max = 0
+		self.leftChild = None
+		self.rightChild = None
+
+class IntervalTree :
+	def __init__(self, t = None) :
+		self.root = t
+
+	def insert(self,root, x=None) :
+		if self.root == None :
+			self.root=x
+			
+		else:
+			l = root.low
+
+			if x.low < l :
+				if root.leftChild==None:
+					root.leftChild=x
+					x.parent=root
+				else:
+					self.insert(root.leftChild,x)
+			else :
+				if root.rightChild==None:
+					root.rightChild=x
+					x.parent=root
+				else:
+					self.insert(root.rightChild,x)
+		if x.max < x.high :
+			x.max = x.high
+			self.updateMax(x)
+
+	def updateMax(self,x):
+		p=x.parent
+		while p!=None:
+			if p.max<x.max:
+				p.max=x.max
+			p=p.parent
+
+	def doOverlap(self,i1, i2) :
+		if i1.low <= i2.high and i2.low <=i1.high :
+			return True
+		return False
+
+
+	def overlapSearch(self,root, i) :
+		if root == None :
+			return None
+		if self.doOverlap(root.i, i) :
+			return root.i
+		if root.leftChild != None and root.leftChild.max >= i.low :
+			return self.overlapSearch(root.leftChild, i)
+
+		return self.overlapSearch(root.rightChild, i)
+
+
+	def inorder(self,root) :
+		if root == None :
+			return root
+
+		self.inorder(root.leftChild)
+		print("[" + str(root.low) + ", " + str(root.high) + "]" + "max = " + str(root.max))
+		self.inorder(root.rightChild)
+
+	def preorder(self,root) :
+		if root == None :
+			return root
+
+		print("[" + str(root.low) + ", " + str(root.high) + "]" + "max = " + str(root.max))
+		self.preorder(root.leftChild)
+		self.preorder(root.rightChild)
+
+
+def main() :
+	e = [[15, 20], [10, 30], [17, 19],[5, 20], [12, 15], [30, 40],[4,10],[2,5] ]
+	rat = IntervalTree()
+	for j in e :
+		rat.insert(rat.root,ITNode(j[0], j[1]))
+	rat.inorder(rat.root)
+	print(rat.root.low,rat.root.high)
+	rat.preorder(rat.root)
+
+if __name__ == '__main__':
+	main()
