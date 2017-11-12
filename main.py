de48505b3b9ec6451c6827204fb4d176d02337e4
Nihar Chitnis <niharchitnis@gmail.com>
2017-11-12 10:53:36 +0530

    Main + Pygame

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-class ITNode :
-	def __init__(self, a = None, b = None) :
+from trial2 import Character
+import os, sys
+import pygame
+
+
+class ITNode:
+	def __init__(self,a,b):
 		self.parent=None
 		self.low = a
 		self.high = b
@@ -8,9 +13,92 @@ class ITNode :
 		self.rightChild = None
 
 
-class IntervalTree :
-	def __init__(self, t = None) :
-		self.root = t
+class IntervalTree:
+
+	def __init__(self):
+		self.root=None
+
+
+	def search(self,x,i):
+		if self.root==None or x==None:
+			return None
+		if i[0]==x.low and i[1]==x.high:
+			return x
+		if i[0]<x.low:
+			return self.search(x.leftChild,i)
+		else:
+			return self.search(x.rightChild,i)
+
+
+	def maximum(self,x):
+		while x.rightChild!=None:
+			x=x.rightChild
+		return x
+
+	
+	def predecessor(self,i):
+		x=self.search(self.root,i)
+		if x==None:
+			return None
+		if x.leftChild!=None:
+			return self.maximum(x.leftChild)
+		y=x.parent
+		while y!=None and x!=y.rightChild:
+			x=y
+			y=y.parent
+		if y==None:
+			return None
+		else:
+			return y
+
+	def doOverlap(self,i1,i2) :
+		if i1[0] <= i2[1] and i2[0] <=i1[1] :
+			return True
+		return False
+
+
+	def overlapSearch(self,root, i) :
+		if root == None :
+			return None
+		if self.doOverlap([root.low,root.high],i) :
+			return [root.low,root.high]
+		if root.leftChild != None and root.leftChild.max >= i[0] :
+			return self.overlapSearch(root.leftChild, i)
+
+		return self.overlapSearch(root.rightChild,i)
+
+	def height(self,z):
+		hl=hr=0
+		if z==None:
+			return 0
+		if z.leftChild==None and z.rightChild==None:
+			return 1
+		else:
+			if z.leftChild!=None:
+				hl=self.height(z.leftChild)
+			if z.rightChild!=None:
+				hr=self.height(z.rightChild)
+		if hl>=hr:
+			return hl+1
+		else:
+			return hr+1
+
+	def updateMax(self,x):
+		a=0
+		b=0
+		if x.leftChild!=None:
+			self.updateMax(x.leftChild)
+			a=x.leftChild.max
+		if x.rightChild!=None:
+			self.updateMax(x.rightChild)
+			b=x.rightChild.max
+		if a>b:
+			x.max=a
+		else:
+			x.max=b
+		if x.high>x.max:
+			x.max=x.high
+
 
 	def insert(self,root, x=None) :
 		if self.root == None :
@@ -32,34 +120,105 @@ class IntervalTree :
 				else:
 					self.insert(root.rightChild,x)
 
-		if x.max < x.high :
-			x.max = x.high
-			self.updateMax(x)
+		self.compare(x)
+		
+		
+		
+		
+	def compare(self,z):
+		while z!=None:
+			hl=self.height(z.leftChild)
+			hr=self.height(z.rightChild)
+			if abs(hl-hr)<=1:
+				z=z.parent
+			elif hl>=hr:
+				y=z.leftChild
+				h1=self.height(y.leftChild)
+				h2=self.height(y.rightChild)
+				if h1>=h2:							#Case 1
+					self.rotateright(y,z)
+					z=y.parent
 
-	def updateMax(self,x):
-		p=x.parent
-		while p!=None:
-			if p.max<x.max:
-				p.max=x.max
-			p=p.parent
+				else:								#Case 3
+					x=y.rightChild
+					self.rotateleft(x,y)
+					self.rotateright(x,z)
+					z=x.parent
 
-	def doOverlap(self,i1,i2) :
-		if i1[0] <= i2[1] and i2[0] <=i1[1] :
-			return True
-		return False
+			else:
+				y=z.rightChild
+				h1=self.height(y.leftChild)
+				h2=self.height(y.rightChild)
+				if h1<=h2:							#Case 2
+					self.rotateleft(y,z)
+					z=y.parent
 
+				else:								#Case 4
+					x=y.leftChild
+					self.rotateright(x,y)
+					self.rotateleft(x,z)
+					z=x.parent
+		while self.root.parent!=None:
+			self.root=self.root.parent
+			self.compare(z)
+				
+	def rotateleft(self,y,z):
+		z.rightChild=y.leftChild
+		if y.leftChild!=None:
+			y.leftChild.parent=z
+		y.leftChild=z
+		y.parent=z.parent
+		if z.parent!=None:
+			if z.parent.rightChild==z:
+				z.parent.rightChild=y
+			else:
+				z.parent.leftChild=y
+		z.parent=y
 
-	def overlapSearch(self,root, i) :
-		if root == None :
-			return None
-		if self.doOverlap([root.low,root.high],i) :
-			return [root.low,root.high]
-		if root.leftChild != None and root.leftChild.max >= i[0] :
-			return self.overlapSearch(root.leftChild, i)
+	def rotateright(self,y,z):
+		z.leftChild=y.rightChild
+		if y.rightChild!=None:
+			y.rightChild.parent=z
+		y.rightChild=z
+		y.parent=z.parent
+		if z.parent!=None:
+			if z.parent.rightChild==z:
+				z.parent.rightChild=y
+			else:
+				z.parent.leftChild=y
+		z.parent=y
+		
 
-		return self.overlapSearch(root.rightChild,i)
-
-
+	def delete(self,i):
+		x=self.search(self.root,i)
+		if x.leftChild==None and x.rightChild==None:
+			y=x.parent
+			z=y
+			if y.leftChild==x:
+				y.leftChild=None
+			else:
+				y.rightChild=None
+		else:
+			if x.leftChild==None:
+				if x.parent.leftChild==x:
+					x.parent.leftChild=x.rightChild
+				else:
+					x.parent.rightChild=x.rightChild
+				x.rightChild.parent=x.parent
+			elif x.rightChild==None:
+				if x.parent.leftChild==x:
+					x.parent.leftChild=x.leftChild
+				else:
+					x.parent.rightChild=x.leftChild
+				x.leftChild.parent=x.parent
+			else:
+				y=self.predecessor([x.low,x.high])
+				self.delete([y.low,y.high])
+				x.low=y.low
+				x.high=y.high
+			z=x
+		self.compare(z)
+		self.updateMax(self.root)
 
 	def inorder(self,root) :
 		if root == None :
@@ -68,7 +227,6 @@ class IntervalTree :
 		self.inorder(root.leftChild)
 		print("[" + str(root.low) + ", " + str(root.high) + "]" + "max = " + str(root.max))
 		self.inorder(root.rightChild)
-
 
 	def preorder(self,root) :
 		if root == None :
@@ -79,15 +237,59 @@ class IntervalTree :
 		self.preorder(root.rightChild)
 
 
+class GUI :
+	def __init__(self) :
+		self.white = (255, 255, 255)
+		self.blue = (0, 0, 255)
+		self.Player = Character((0, 0, 3, 150))
+		self.Surface = pygame.display.set_mode((1000, 600))
+		self.MyClock = pygame.time.Clock()
+
+
+	def initial(self) :
+		os.environ['SDL_VIDEO_CENTERED'] = '1'
+		pygame.init()
+		self.Player.rect.center = self.Surface.get_rect().center
+
+	def loop(self) :
+		self.font = pygame.font.SysFont('Comic Sans MS', 30)
+		self.Player.game_event_loop(self.Player)
+		self.Surface.fill(self.white)
+		rect = pygame.Rect(200, 150, 100, 50)
+		image = pygame.Surface(rect.size)
+		image.fill((0, 0, 255))
+		self.Surface.blit(image, rect)
+		bottomleft = rect.bottomleft
+		bottomright = rect.bottomright
+		topleft = rect.topleft
+		topright = rect.topright
+		t = self.Player.update(self.Surface)
+		answer = self.font.render(self.Player.text, False, (0, 255, 0))
+		self.Surface.blit(answer, (500, 100))
+		textsurface = self.font.render(str(t) + str(bottomleft) + str(bottomright) + str(topleft) + str(topright), False, (0, 0, 0))
+		self.Surface.blit(textsurface, (0, 0))
+
+	
 def main() :
-	e = [[15, 20], [10, 30], [17, 19],[5, 20], [12, 15], [30, 40]]
+
+	pg = GUI()
+	pg.initial()
+
+	e = [[15, 20], [10, 30], [17, 19],[5, 20], [12, 15], [30, 40],[4,10],[2,6]]
 	rat = IntervalTree()
 	for j in e :
 		rat.insert(rat.root,ITNode(j[0], j[1]))
+	rat.updateMax(rat.root)
 	rat.inorder(rat.root)
 	print(rat.root.low,rat.root.high)
 	rat.preorder(rat.root)
-	print(rat.overlapSearch(rat.root,[6,7]))
+	print(rat.overlapSearch(rat.root,[-1,3]))
+
+
+	while True :
+		pg.loop()
+		pygame.display.update()
+		pg.MyClock.tick(60)
 
 if __name__ == '__main__':
 	main()
---
+++ trial2.py
@@ -0,0 +1,56 @@
+import os, sys
+import pygame
+
+class Character :
+	def __init__(self, rect) :
+		self.rect = pygame.Rect(rect)
+		self.click = False
+		self.image = pygame.Surface(self.rect.size)
+		self.image.fill((255, 0, 0))
+		self.text = 'NO'
+
+	def update(self, surface) :
+		if self.click :
+			self.rect.center = pygame.mouse.get_pos()
+		x = self.rect.topleft
+		y = self.rect.bottomleft
+		a = [x, y]
+		surface.blit(self.image, self.rect)
+		return a
+
+	def game_event_loop(self, Player) :
+		for event in pygame.event.get() :
+			if event.type == pygame.MOUSEBUTTONDOWN :
+				if Player.rect.collidepoint(event.pos) :
+					Player.click = True
+			elif event.type == pygame.MOUSEBUTTONUP :
+				Player.click == False
+			elif event.type == pygame.QUIT or event.type == pygame.K_ESCAPE :
+				pygame.quit()
+				sys.exit()
+
+def main() :
+	os.environ['SDL_VIDEO_CENTERED'] = '1'
+	pygame.init()
+	font = pygame.font.SysFont('Comic Sans MS', 30)
+	Surface = pygame.display.set_mode((1000, 600))
+	MyClock = pygame.time.Clock()
+	Player = Character((0, 0, 10, 150))
+	Player.rect.center = Surface.get_rect().center
+	while True :
+		white = (255, 255, 255)
+		blue = (0, 0, 255)
+		game_event_loop(Player)
+		Surface.fill(white)
+		pygame.draw.rect(Surface,blue,(200,150,100,50))
+		t = Player.update(Surface)
+		textsurface = font.render(str(t), False, (0, 0, 0))
+		Surface.blit(textsurface, (0, 0))
+		pygame.display.update()
+		MyClock.tick(60)
+
+
+
+
+if __name__ == '__main__':
+	main()
